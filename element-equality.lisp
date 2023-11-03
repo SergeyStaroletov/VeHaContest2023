b813692cc12cb15dc95c0e81908ca4a8dbd58203
IlyaAAAA <ilya-sachuk@mail.ru>
2023-11-03 17:53:45 +0300

    added

---
+++ element-equality.lisp
@@ -0,0 +1,53 @@
+(in-package "ACL2")
+
+(include-book "std/util/defrule" :dir :system)
+(include-book "centaur/fty/top" :dir :system)
+(include-book "std/util/bstar" :dir :system)
+(include-book "std/typed-lists/top" :dir :system)
+(include-book "std/lists/top" :dir :system)
+(include-book "std/basic/inductions" :dir :system)
+
+(defun element-equality(i n a)
+    (if
+        (or
+            (not
+                (natp
+                    i
+                )
+            )
+            (not
+                (natp
+                    n
+                )
+            )
+        )
+        0
+        (if
+            (>=
+                i
+                n
+            )
+            1
+
+            (if
+                (=
+                    (nth
+                        i a
+                    )
+                    (nth
+                        n a
+                    )
+                )
+                (element-equality
+                    i
+                    (-
+                        n
+                        1
+                    )
+                    a
+                )
+                0
+            )
+        )
+    )
+)
