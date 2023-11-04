5f045981d346125415e18e974c78c4c3c296e84c
Victor <72170942+rokajero@users.noreply.github.com>
2023-11-04 20:50:20 +0300

    Added deductive_ver

---
+++ deductive_ver/element-eq.lisp
@@ -0,0 +1,20 @@
+(in-package "ACL2")
+
+(include-book "std/util/defrule" :dir :system)
+(include-book "centaur/fty/top" :dir :system)
+(include-book "std/util/bstar" :dir :system)
+(include-book "std/typed-lists/top" :dir :system)
+(include-book "std/lists/top" :dir :system)
+(include-book "std/basic/inductions" :dir :system)
+
+(defun element-eq(i j a)
+    (if (or (not (natp i)) (not (natp j))) 0
+        (if (> i j) 0
+            (if (= i j) 1
+                (if (not (equal (nth (- j 1) a) (nth j a))) 0
+                    (element-eq i (- j 1) a)
+                )
+            )
+        )
+    )
+)
