d5e9c32a3d3271152f2893934419d9a8f514dd47
danianmercado <danianmercado@gmail.com>
2023-11-04 11:32:15 +0300

    initial commit

---
+++ 5140904-20202/deductive-verification/element_equality.c
@@ -0,0 +1,14 @@
+/* (and (integer-listp a) (natp n) (< 0 n) (<= n (length a))) */
+int element_equality(int *a, int n){
+    int result = 1;
+    for (int i = 1; i < n; i++)
+	{
+        if (a[i-1] != a[i])
+		{
+		    result = 0;
+			break;
+		}
+	}
+    return result;
+}
+/* */
